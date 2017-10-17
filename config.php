c997fc4fca3f6abbd46500c31f7283d92e515236
BBC-BE\bhunzb <benhu00@outlook.com>
2017-10-17 15:37:16 +0200

    test

---
+++ config.php
@@ -0,0 +1,11 @@
+return array(
+
+    // Datebankkonfiguration
+    'database' => array(
+        'host'     => 'DATABASE_HOST',
+        'username' => 'DATABASE_USER',
+        'password' => 'DATABASE_PASSWORD',
+        'database' => 'DATABASE_NAME',
+    ),
+
+);
