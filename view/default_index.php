73fb1067fbf068b53a57f3554d2d0451d44d6b8b
noahporfido <noah.porfido@icloud.com>
2017-10-19 14:27:09 +0200

    added script

--- view/default_index.php
+++ view/default_index.php
@@ -1,7 +1,7 @@
 <div id="StartseiteContainer">
     <p id="StartseiteText">fakestagram</p>
     <div>
-        <input id="BenutzernameLabel" type="text" placeholder="Enter Username" name="uname" required>
+        <input id="BenutzernameLabel" onclick="removePlaceholder(this)" type="text" placeholder="Enter Username" name="uname" required>
     </div>
     <br/>
     <input id="PasswortLabel" type="password" placeholder="Enter Password" name="psw" required>
