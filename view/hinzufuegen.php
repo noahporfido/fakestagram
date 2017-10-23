97a0a5f8fd04983c81a4c45ce93b417d59f69010
celinemo <celineanne01@gmail.com>
2017-10-23 13:24:43 +0200

    swag
    hinzufügen html/css + controller

---
+++ view/hinzufuegen.php
@@ -0,0 +1,14 @@
+<div id="hinzufuegenContainer">
+<p id ="textHinzufuegen">Neues Bild hinzufügen</p>
+<div>
+<form action= "" method="post" >
+<input type = text id= "inputName" placeholder="Name" onclick="removePlaceholder(this)">
+<br/>
+<input type = text id ="inputBeschreibung" placeholder="Beschreibung" onclick="removePlaceholder(this)">
+</br>
+<input type = image id="inputBild">
+</br>
+<input type = submit id="inputPosten" >
+</form>
+</div>
+</div>
