d4987e8196349dc340968ee45f98d90e02c4671c
noah.porfido <noah.porfido@icloud.com>
2017-10-23 14:30:39 +0200

    edited edit page

--- view/edit.php
+++ view/edit.php
@@ -2,12 +2,18 @@
     <img id="ButtonBack" src="/images/back_arrow.png">
 </div>
 <div id="EditField">
-    <div class="BildEdit">
-    </div>
-    <form>
-        <input id="NameInput" type="text" name="name"><br>
-        <textarea id="BeschreibungInput" name="beschreibung" rows="20" cols="50"></textarea>
-        <input id="SaveInput" type="submit" name="save" value="Speichern"><br>
-        <input id="DeleteInput" type="submit" name="delete" value="Löschen"><br>
-    </form>
+    <?php
+   echo "<div class='BildEdit' id='bild$bild->id'
+    </div>";
+       echo "<style type='text/css'>";
+                echo "#bild$bild->id {
+                    background-image: url('/uploadimages/$bild->bild'); 
+                }</style>";
+        ?>
+        <form>
+            <input id="NameInput" type="text" name="name" placeholder="Name"><br>
+            <textarea id="BeschreibungInput" name="beschreibung" rows="20" cols="50" placeholder="Comment..."></textarea>
+            <input id="SaveInput" type="submit" name="save" value="Speichern"><br>
+            <input id="DeleteInput" type="submit" name="delete" value="Löschen"><br>
+        </form>
 </div>
