4b41cb640aad6d3fc7a85be305dbdc2480559765
Hunzinger Benaja <bhunzb@bbc-be.local>
2017-10-30 13:26:46 +0100

    repair

--- view/edit.php
+++ view/edit.php
@@ -12,26 +12,12 @@
                     background-image: url('/uploadimages/$bild->bild'); 
                 }</style>";
         ?> <!--Information get sent to BilderController-->
-<<<<<<< HEAD
-=======
-        <form action='<?php echo "/bild/update?id=$bild->id" ?>' method="post">
-            <input id="NameInput" type="text" required name="name" maxlength="20" value="<?php echo $bild->name; ?>"><br>
-            <textarea id="BeschreibungInput" maxlength="100" name="beschreibung" rows="20" cols="50"><?php echo 
-        ?>
->>>>>>> 3ca86b9334a9df6165a4b7b7a3eb51f4df8f82b4
         <div id="eingabeelemente">
             <form action='<?php echo "/bild/update?id=$bild->id" ?>' method="post">
                 <p id="name_edit" class="edit_beschreibung">Name</p>
                 <input id="NameInput" type="text" required name="name" maxlength="20" value="<?php echo $bild->name; ?>">
                 <p id="beschreibung_edit" class="edit_beschreibung">Beschreibung</p>
                 <textarea id="BeschreibungInput" maxlength="100" name="beschreibung" rows="20" cols="50"><?php echo $bild->beschreibung; ?></textarea>
-<<<<<<< HEAD
-=======
-=======
-                <textarea id="BeschreibungInput" maxlength="100" name="beschreibung" rows="20" cols="50"><?php echo 
-            $bild->beschreibung; ?></textarea>
->>>>>>> ebd20a1dbc00a4075113f8e420f6d49b96c359ff
->>>>>>> 3ca86b9334a9df6165a4b7b7a3eb51f4df8f82b4
                 <input id="SaveInput" type="submit" name="save" value="Speichern">
             </form>
         </div>
